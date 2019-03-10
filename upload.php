716a59aaa25bbcb6e324fc5c3d341db9ab1c12bc
James Hawkins <jhawki2005@gmail.com>
2019-03-10 21:09:03 +0000

    t

--- upload.php
+++ upload.php
@@ -1,17 +1,11 @@
 <?php
-<<<<<<< HEAD
-$secret_key = "1m@g3h0sTinG"; // Secret key, to prevent other people from uploading
-$sharexdir = "files/"; // Directory for images
-$domain_url = 'https://thedudeinthecorner.xyz/'; // URL of the image site
-=======
-$secret_key = "imagehosting"; //Set this as your secret key, to prevent others uploading to your server.
+$secret_key = "1m@g3h0sTinG"; //Set this as your secret key, to prevent others uploading to your server.
 $sharexdir = "files/"; //This is your file dir, also the link..
 $domain_url = 'https://thedudeinthecorner.xyz/';
->>>>>>> parent of 7e4182b... stuff
 $lengthofstring = 5; //Length of the file name
  
-function generateRandomString($length = 10) { // Generate a random string for the image URL
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
     for ($i = 0; $i < $length; $i++) {
@@ -31,15 +25,10 @@ if(isset($_POST['secret']))
         if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir.$filename.'.'.$fileType))
         {
             echo $domain_url.$sharexdir.$filename.'.'.$fileType;
-<<<<<<< HEAD
-        } else {
-           echo 'File upload failed - configured folder does not exist';
-=======
         }
             else
         {
            echo 'File upload failed - CHMOD/Folder doesn\'t exist?';
->>>>>>> parent of 7e4182b... stuff
         }  
     }
     else
@@ -50,9 +39,5 @@ if(isset($_POST['secret']))
 else
 {
     echo 'Failed to upload, reason unknown';
-<<<<<<< HEAD
-} ?>
-=======
 }
 ?>
->>>>>>> parent of 7e4182b... stuff
