7e4182b0d6b70ea36973f95df7f9c756414c7c53
James Hawkins <jhawki2005@gmail.com>
2019-03-10 20:56:36 +0000

    stuff

--- upload.php
+++ upload.php
@@ -1,43 +1,37 @@
 <?php
-$secret_key = "imagehosting"; //Set this as your secret key, to prevent others uploading to your server.
-$sharexdir = "files/"; //This is your file dir, also the link..
-$domain_url = 'https://thedudeinthecorner.xyz/';
+include config.php
+
+$secret_key = echo $secretKey; // Secret key, to prevent other people from uploading
+$sharexdir = "files/"; // Directory for images
+$domain_url = 'https://thedudeinthecorner.xyz/'; // URL of the image site
 $lengthofstring = 5; //Length of the file name
  
 function generateRandomString($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
+
     for ($i = 0; $i < $length; $i++) {
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
 }
  
-if(isset($_POST['secret']))
-{
-    if($_POST['secret'] == $secret_key)
-    {
+if(isset($_POST['secret'])) {
+    if($_POST['secret'] == $secret_key) {
         $filename = generateRandomString($lengthofstring);
         $target_file = $_FILES["sharex"]["name"];
         $fileType = pathinfo($target_file, PATHINFO_EXTENSION);
  
-        if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir.$filename.'.'.$fileType))
-        {
+        if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir.$filename.'.'.$fileType)) {
             echo $domain_url.$sharexdir.$filename.'.'.$fileType;
-        }
-            else
-        {
-           echo 'File upload failed - CHMOD/Folder doesn\'t exist?';
+        } else {
+           echo 'File upload failed - configured folder doesn''t exist';
         }  
+    } else {
+        echo 'Your key is invalid!';
     }
-    else
-    {
-        echo 'Invalid Secret Key';
-    }
-}
-else
-{
+} else {
     echo 'Failed to upload, reason unknown';
 }
 ?>
