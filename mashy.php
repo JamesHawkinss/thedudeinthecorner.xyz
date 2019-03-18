74e0d7ab72b4c0f24731d75ca235cbdad42b2367
James Hawkins <jhawki2005@gmail.com>
2019-03-18 21:19:53 +0000

    segregation

---
+++ mashy.php
@@ -0,0 +1,34 @@
+<?php
+$secret_key = "key"; //Set this as your secret key, to prevent others uploading to your server.
+$sharexdir = "mashhhyyy/"; //This is your file dir, also the link..
+$domain_url = 'https://thedudeinthecorner.xyz/';
+$lengthofstring = 5; //Length of the file name
+ 
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+ 
+if(isset($_POST['secret'])) {
+    if($_POST['secret'] == $secret_key) {
+        $filename = generateRandomString($lengthofstring);
+        $target_file = $_FILES["sharex"]["name"];
+        $fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+ 
+        if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir.$filename.'.'.$fileType)) {
+            echo $domain_url.$sharexdir.$filename.'.'.$fileType;
+        } else {
+           echo 'File upload failed - CHMOD/Folder doesn\'t exist?';
+        }  
+    } else {
+        echo 'Upload failed - Invalid Secret Key';
+    }
+} else {
+    echo 'Upload failed - Please use ShareX';
+}
+?>
